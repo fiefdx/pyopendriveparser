1e86a66df46a4a11b4ce138ee2b0617a03e59ffb
Stefan Urban <stefan.urban@live.de>
2017-09-05 13:48:57 +0200

    updated setup

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(name='opendriveparser',
       version='1.0',
@@ -8,7 +8,7 @@ setup(name='opendriveparser',
       author='Stefan Urban',
       author_email='stefan.urban@live.de',
       url='https://github.com/stefan-urban/pyopendriveparser',
-      packages=['opendriveparser'],
+      packages=find_packages(),
       dependency_links=[
           'https://github.com/stefan-urban/pyeulerspiral/archive/master.zip#egg=pyeulerspiral-1.0',
       ],
